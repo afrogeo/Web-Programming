9892027785decf225a993361d78b683e38332d03
afrogeo <54954023+afrogeo@users.noreply.github.com>
2020-11-15 03:17:08 -0500

    Add files via upload

---
+++ hw10/processForm.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title> Process Form </title>
+            <script language="javascript">
+
+            </script>
+        
+    </head>
+    <body>
+        <?php
+            $fn = $_POST["fname"];
+            $ln = $_POST["lname"];
+            echo ("The name is $fn $ln <br>");
+        ?>
+    </body>
+</html>
